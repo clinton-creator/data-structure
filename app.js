d1d3055f1c84e0fb46f6fead94b6167798a4e872
clinton woko <clinton.woko20@gmail.com>
2025-05-18 09:43:03 +0100

    first commit

--- app.js
+++ app.js
@@ -146,10 +146,75 @@
 
 // checkOrthogonality(vectorPairs);
 
-function addition(a, b) {
-  return a + b;
+// function addition(a, b) {
+//   return a + b;
+// }
+
+// console.log(addition(3, 2)); // ➞ 5
+// console.log(addition(-3, -6)); // ➞ -9
+// console.log(addition(7, 3)); // ➞ 10
+
+function reverseString(str) {
+  return str.split("").reverse().join("");
 }
 
-console.log(addition(3, 2)); // ➞ 5
-console.log(addition(-3, -6)); // ➞ -9
-console.log(addition(7, 3)); // ➞ 10
+function countCharacters(str) {
+  return str.length;
+}
+
+function capitalizeWords(str) {
+  return str
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
+
+function findMax(arr) {
+  return Math.max(...arr);
+}
+
+function findMin(arr) {
+  return Math.min(...arr);
+}
+
+function sumArray(arr) {
+  return arr.reduce((sum, num) => sum + num, 0);
+}
+
+function filterArray(arr, conditionFn) {
+  return arr.filter(conditionFn);
+}
+
+const numbers = [1, 2, 3, 4, 5];
+const evenNumbers = filterArray(numbers, (num) => num % 2 === 0);
+console.log(evenNumbers); // Output: [2, 4]
+
+function factorial(n) {
+  if (n < 0) return undefined; // Factorial is not defined for negative numbers
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+function isPrime(num) {
+  if (num <= 1) return false;
+  if (num === 2) return true;
+  if (num % 2 === 0) return false;
+  const sqrt = Math.sqrt(num);
+  for (let i = 3; i <= sqrt; i += 2) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+function generateFibonacci(n) {
+  if (n <= 0) return [];
+  if (n === 1) return [0];
+  const fib = [0, 1];
+  for (let i = 2; i < n; i++) {
+    fib.push(fib[i - 1] + fib[i - 2]);
+  }
+  return fib;
+}
